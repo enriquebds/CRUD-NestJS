671acbf4a25b40834e9d1634ca82ee3572c9d8f2
Enrique <enrique.barbosasilva@gmai.com>
2023-04-26 23:35:46 -0300

    new methods and interface added

--- src/user/user.interfaces.ts
+++ src/user/user.interfaces.ts
@@ -1,5 +1,10 @@
 export interface CreateUserRequest {
+    id: string
     name: string;
     email: string;
     password: string;
+}
+
+export interface User {
+
 }
--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,7 +1,30 @@
+import { CreateUserRequest, User } from "./user.interfaces";
+import { randomUUID } from "crypto";
+
 export class UserRepository {
     private users = [];
 
-    async save(user: any) {
-        this.users.push(user)
+    async create(user: CreateUserRequest) {
+
+        const body = {
+            id: randomUUID(),
+            email: user.email,
+            name: user.name,
+            password: user.password,
+        }
+
+        this.users.push(body)
+    }
+
+    async list (): Promise<User[]> {
+        return this.users
+    }
+
+    async listById (id: string): Promise<User> {
+        const findUser = this.users.find(user => user.id === id)
+
+        console.log(findUser)
+
+        return findUser
     }
 }
